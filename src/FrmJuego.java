f41a80fb506827735093baf9f3e27eaba5e7de44
JuanLopera0726 <juanmanuel07266@gmail.com>
2026-03-15 23:51:22 -0500

    Se agrega funcionalidad de escalera y puntuacion

--- src/FrmJuego.java
+++ src/FrmJuego.java
@@ -13,60 +13,91 @@ public class FrmJuego extends JFrame {
 
     JTabbedPane tpJugadores;
 
-    public FrmJuego(){
+    public FrmJuego() {
         setSize(500, 300);
         setTitle("Juguemos al apuntado");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(null);
 
         JButton btnRepartir = new JButton("Repartir");
-        btnRepartir.setBounds(10,10,100,25);
+        btnRepartir.setBounds(10, 10, 100, 25);
         add(btnRepartir);
 
-        JButton btnVerificar = new JButton("Verificar");
-        btnVerificar.setBounds(120,10,100,25);
+        JButton btnVerificar = new JButton("Grupos");
+        btnVerificar.setBounds(120, 10, 100, 25);
         add(btnVerificar);
 
+        JButton btnEscalera = new JButton("Escalera");
+        btnEscalera.setBounds(230, 10, 100, 25);
+        add(btnEscalera);
+
+        JButton btnPuntuacion = new JButton("Puntuación");
+        btnPuntuacion.setBounds(340, 10, 100, 25);
+        add(btnPuntuacion);
+
         tpJugadores = new JTabbedPane();
-        tpJugadores.setBounds(10,50,460,200);
+        tpJugadores.setBounds(10, 50, 460, 200);
         add(tpJugadores);
-        
+
         // Pestañas
         pnlJugador1 = new JPanel();
-        pnlJugador1.setBackground(new Color(0,255,0));
+        pnlJugador1.setBackground(new Color(128, 0, 128));
 
         pnlJugador2 = new JPanel();
-        pnlJugador2.setBackground(new Color(0,255,255));
+        pnlJugador2.setBackground(new Color(0, 187, 45));
 
         tpJugadores.addTab("Juan Manuel Lopera", pnlJugador1);
         tpJugadores.addTab("Salomé Ceballos Cardona", pnlJugador2);
 
         // Eventos
-        btnRepartir.addActionListener(e ->{
+        btnRepartir.addActionListener(e -> {
             repartir();
         });
-        
-         btnVerificar.addActionListener(e ->{
-            verificar();
+
+        btnVerificar.addActionListener(e -> {
+            verificarGrupos();
+        });
+
+        btnEscalera.addActionListener(e -> {
+            verificarEscalera();
+        });
+
+        btnPuntuacion.addActionListener(e -> {
+            puntuacion();
         });
     }
 
-    private void repartir(){
-       jugador1.repartir();
-       jugador2.repartir();
+    private void repartir() {
+        jugador1.repartir();
+        jugador2.repartir();
 
-       jugador1.mostrar(pnlJugador1);
-       jugador2.mostrar(pnlJugador2);
-       
+        jugador1.mostrar(pnlJugador1);
+        jugador2.mostrar(pnlJugador2);
+
     }
 
-    private void verificar(){
-        if (tpJugadores.getSelectedIndex() == 0){
+    private void verificarGrupos() {
+        if (tpJugadores.getSelectedIndex() == 0) {
             JOptionPane.showMessageDialog(null, jugador1.getGrupos());
-        } else{
+        } else {
             JOptionPane.showMessageDialog(null, jugador2.getGrupos());
         }
     }
 
+    private void verificarEscalera() {
+        if (tpJugadores.getSelectedIndex() == 0) {
+            JOptionPane.showMessageDialog(null, jugador1.getEscalera());
+        } else {
+            JOptionPane.showMessageDialog(null, jugador2.getEscalera());
+        }
+    }
+
+    private void puntuacion() {
+        if (tpJugadores.getSelectedIndex() == 0) {
+            JOptionPane.showMessageDialog(null, jugador1.getPuntuacion());
+        } else {
+            JOptionPane.showMessageDialog(null, jugador2.getPuntuacion());
+        }
+    }
 
 }
--- src/Jugador.java
+++ src/Jugador.java
@@ -12,38 +12,137 @@ public class Jugador {
     private Carta[] cartas = new Carta[TOTAL_CARTAS];
     private Random r = new Random();
 
-    public void repartir(){
+    public void repartir() {
         for (int i = 0; i < cartas.length; i++) {
             cartas[i] = new Carta(r);
         }
     }
 
-    public void mostrar(JPanel pnl){
+    public void mostrar(JPanel pnl) {
         pnl.removeAll();
         pnl.setLayout(null);
-        int posicion = MARGEN_IZQUIERDA + DISTANCIA_CARTAS*(TOTAL_CARTAS-1);
-        for (Carta carta : cartas){
-            carta.mostrar( posicion, MARGEN_SUPERIOR, pnl);
+        int posicion = MARGEN_IZQUIERDA + DISTANCIA_CARTAS * (TOTAL_CARTAS - 1);
+        for (Carta carta : cartas) {
+            carta.mostrar(posicion, MARGEN_SUPERIOR, pnl);
             posicion -= DISTANCIA_CARTAS;
         }
         pnl.repaint();
     }
 
-    public String getGrupos(){
+    public String getGrupos() {
         int[] contadores = new int[NombreCarta.values().length];
 
-        for (Carta carta : cartas){
+        for (Carta carta : cartas) {
             // Oirdinal para enumerados
             contadores[carta.getNombre().ordinal()]++;
         }
 
         String grupos = "";
-        for (int i=0; i< contadores.length; i++){
-            if (contadores[i]>=2){
-                grupos += Grupo.values()[contadores[i]]+" DE "+ NombreCarta.values()[i]+ "\n";
+        for (int i = 0; i < contadores.length; i++) {
+            if (contadores[i] >= 2) {
+                grupos += Grupo.values()[contadores[i]] + " DE " + NombreCarta.values()[i] + "\n";
             }
         }
-        return grupos.isEmpty() ? "No hay grupos" : "Se encontraron los siguientes grupos:\n"+grupos;
+        // devuelve el resultado si hay o no grupos
+        return grupos.isEmpty() ? "No hay grupos" : "Se encontraron los siguientes grupos:\n" + grupos;
+    }
+
+    public String getEscalera() {
+        // Matriz para ordenar por pinta y valor
+        int[][] contadores = new int[Pinta.values().length][NombreCarta.values().length];
+        // contar cartas por pinta y valor
+        for (Carta carta : cartas) {
+            int pinta = carta.getPinta().ordinal();
+            int valor = carta.getNombre().ordinal();
+            contadores[pinta][valor]++;
+        }
+        // resultado y booleano para saber si hay escaleras
+        String resultado = "Se encontraron las siguientes escaleras:\n";
+        int  hayEscalera = 0;
+        // recorrer cada pinta
+        for (int i = 0; i < contadores.length; i++) {
+            // inicio y longitud de la escalera
+            int inicio = -1;
+            int longitud = 0;
+            // recorrer cada valor en cada pinta
+            for (int j = 0; j < contadores[i].length; j++) {
+                // Si hay cartas de la misma pinta
+                if (contadores[i][j] > 0) {
+                    if (inicio == -1) {
+                        inicio = j;
+                    }
+                    longitud++;
+                } else {
+                    // Preguntamos la longitud para saber si hay una escalera
+                    if (longitud >= 2) {
+                        resultado += Pinta.values()[i] + ": " + NombreCarta.values()[inicio] + " HASTA "
+                                + NombreCarta.values()[j - 1] + "\n";
+                        hayEscalera = 1;
+                    }
+                    inicio = -1;
+                    longitud = 0;
+                }
+            }
+            // por si la esalera termina al final
+            if (longitud >= 2) {
+                resultado += Pinta.values()[i] + ": " + NombreCarta.values()[inicio] + " HASTA "
+                        + NombreCarta.values()[contadores[i].length - 1] + "\n";
+                hayEscalera = 1;
+            }
+        }
+        return hayEscalera == 1 ? resultado : "No hay escaleras en la mano";
+    }
+
+    private int valorCarta(NombreCarta nombre) {
+
+        int posicion = nombre.ordinal();
+        // Si es As,Jack,Queen,King
+        if (posicion >= 9 || posicion == 0) {
+            return 10;
+        }
+        // Si son las cartas normales
+        return posicion + 1;
+    }
+
+    public String getPuntuacion() {
+        int[] cartaUsada = new int[TOTAL_CARTAS];
+        int puntuacion = 0;
+        // detectar grupos
+        for (int i = 0; i < cartas.length; i++) {
+            for (int j = i + 1; j < cartas.length; j++) {
+
+                if (cartas[i].getNombre() == cartas[j].getNombre()) {
+                    cartaUsada[i] = 1;
+                    cartaUsada[j] = 1;
+                }
+            }
+        }
+        // detectar escaleras de la misma pinta
+        for (int i = 0; i < cartas.length; i++) {
+            for (int j = 0; j < cartas.length; j++) {
+                // Si no son iguales
+                if (i != j) {
+                    // Si son de la misma pinta
+                    if (cartas[i].getPinta() == cartas[j].getPinta()) {
+
+                        int carta1 = cartas[i].getNombre().ordinal();
+                        int carta2 = cartas[j].getNombre().ordinal();
+
+                        if (carta1 + 1 == carta2 || carta2 + 1 == carta1) {
+                            cartaUsada[i] = 1;
+                            cartaUsada[j] = 1;
+                        }
+                    }
+                }
+            }
+        }
+        // sumar cartas no usadas
+        for (int i = 0; i < cartas.length; i++) {
+            if (cartaUsada[i] != 1) {
+                puntuacion += valorCarta(cartas[i].getNombre());
+            }
+        }
+        return "Puntuación del jugador: " + puntuacion;
     }
 
 }
